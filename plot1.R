06d3736f26121fe8f6d89c6f8749620d9a527f83
EvaKuo <EvaKuo@users.noreply.github.com>
2016-04-09 21:14:58 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,7 +3,8 @@ ProjectUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_p
 download.file(ProjectUrl, 
               destfile = "./data.zip", method = "curl")
 unzip("data.zip", exdir = "data") ## download and unzip data
-PowerData <- read.table("./data/household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE, header = TRUE, na = "?")
+PowerData <- read.table("./data/household_power_consumption.txt", 
+              sep = ";", stringsAsFactors = FALSE, header = TRUE, na = "?")
 TwoDays <- PowerData[PowerData$Date %in% c("1/2/2007", "2/2/2007"), ] ##subsets data to the 2 days needed
 png(file = "plot1.png", width = 480, height = 480, units = "px") ## opens png graphics device
 plot1 <- hist(TwoDays$Global_active_power, col = "red", 
