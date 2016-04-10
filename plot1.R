f8374ca3bdc77d187518a57ab8d206ee9db22f99
EvaKuo <EvaKuo@users.noreply.github.com>
2016-04-09 21:14:40 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,5 +6,6 @@ unzip("data.zip", exdir = "data") ## download and unzip data
 PowerData <- read.table("./data/household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE, header = TRUE, na = "?")
 TwoDays <- PowerData[PowerData$Date %in% c("1/2/2007", "2/2/2007"), ] ##subsets data to the 2 days needed
 png(file = "plot1.png", width = 480, height = 480, units = "px") ## opens png graphics device
-plot1 <- hist(TwoDays$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
+plot1 <- hist(TwoDays$Global_active_power, col = "red", 
+              xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
 dev.off() ## turn off png device and save file to working directory
